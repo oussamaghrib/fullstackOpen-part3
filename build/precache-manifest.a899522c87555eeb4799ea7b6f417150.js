90bcca09eac4867be7b5083573cd4a4bacd77230
oussamaghrib <oussghrib@gmail.com>
2021-01-15 21:38:18 +0100

    added the build folder

---
+++ build/precache-manifest.a899522c87555eeb4799ea7b6f417150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e74b34d425edec1a91c67efd3e4bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd3a0063a9a8a597e43e",
+    "url": "/static/css/main.f6d3bdad.chunk.css"
+  },
+  {
+    "revision": "1af8362727c1c0958194",
+    "url": "/static/js/2.3a8a7a57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a8a7a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3a0063a9a8a597e43e",
+    "url": "/static/js/main.fff71913.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
