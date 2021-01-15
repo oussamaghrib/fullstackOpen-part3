286eddfc056f59c9f5c35eb46bec0a45d28370b6
oussamaghrib <oussghrib@gmail.com>
2021-01-15 21:26:25 +0100

    changed something

--- index.js
+++ index.js
@@ -97,5 +97,5 @@ app.post("/api/persons", (req, res) => {
   res.send(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
